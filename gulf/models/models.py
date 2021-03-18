31cadb1af8cd0a5483244ff38cac26a36ea623ba
Viltco <58694110+Viltco@users.noreply.github.com>
2021-03-18 16:35:47 +0500

    Add files via upload

---
+++ gulf/models/models.py
@@ -0,0 +1,201 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class GulfCostTyres(models.Model):
+    _inherit = 'product.template'
+
+
+    # reserve_stock_count = fields.Float("Reserve Stock")
+
+    dot = fields.Integer(string="DOT")
+    brand = fields.Char(string='Brand')
+    wavrg = fields.Char(string='Wavrg')
+    sale1 = fields.Char(string='Sale 1 (Walk-in)', compute="compute_sale_walk_in")
+    sale2 = fields.Char(string='Sale 2 (Credit)', compute="compute_sale_credit")
+    sale3 = fields.Char(string='Sale 3 (Cash)', compute="compute_sale_cash")
+
+    # locations
+
+    shj = fields.Float(string='Shj', compute="compute_shj_location")
+    shjsm = fields.Float(string='ShjSM', compute="compute_shjsm_location")
+    quz = fields.Float(string='Quz', compute="compute_quz_location")
+    ain = fields.Float(string='Ain', compute="compute_ain_location")
+    ainw = fields.Float(string='AinW', compute="compute_ainw_location")
+    ajm = fields.Float(string='Ajm', compute="compute_ajm_location")
+    albr = fields.Float(string='AlBr', compute="compute_albr_location")
+    ban = fields.Float(string='BAN', compute="compute_ban_location")
+    abu = fields.Float(string='ABU', compute="compute_abu_location")
+    abuold = fields.Float(string='ABUOLD', compute="compute_abuold_location")
+    mzr = fields.Float(string='MZR', compute="compute_mzr_location")
+
+    res_stk = fields.Float(string="Res-Stk")
+    p_inv = fields.Float(string="P-Inv", compute="compute_product_invoice_sale")
+    pur_qty = fields.Float(string="Pur-Qty", compute="compute_product_invoice_purchase")
+
+    def compute_product_invoice_sale(self):
+        for rec in self:
+            total = 0
+            qty = self.env['stock.picking'].search([])
+            for i in qty:
+                if i.state == 'assigned':
+                    for j in i.move_line_ids_without_package:
+                        if j.product_id.name == rec.name:
+                            total = j.product_uom_qty + total
+            rec.p_inv = total
+
+    def compute_product_invoice_purchase(self):
+        for rec in self:
+            total = 0
+            qty = self.env['stock.picking'].search([])
+            for i in qty:
+                if i.state == 'assigned':
+                    for j in qty.move_ids_without_package:
+                        if j.product_id.name == rec.name:
+                            total = j.product_uom_qty + total
+            rec.pur_qty = total
+
+    def compute_shj_location(self):
+        for k in self:
+            obj = self.env['stock.quant'].search([])
+            for i in obj:
+                print('vbnm')
+                if i.location_id.location_id.name == 'Shj':
+                    k.shj = i.inventory_quantity
+                else:
+                    k.shj
+                    print('vbnm')
+
+    def compute_shjsm_location(self):
+        for k in self:
+            obj = self.env['stock.quant'].search([])
+            for i in obj:
+                if i.location_id.location_id.name == 'ShjSM':
+                    k.shjsm = i.inventory_quantity
+                else:
+                    k.shjsm
+
+    def compute_quz_location(self):
+        for k in self:
+            obj = self.env['stock.quant'].search([])
+            for i in obj:
+                if i.location_id.location_id.name == 'Quz':
+                    k.quz = i.inventory_quantity
+                else:
+                    k.quz
+
+    def compute_ain_location(self):
+        for k in self:
+            obj = self.env['stock.quant'].search([])
+            for i in obj:
+                if i.location_id.location_id.name == 'Ain':
+                    k.ain = i.inventory_quantity
+                else:
+                    k.ain
+
+    def compute_ainw_location(self):
+        for k in self:
+            obj = self.env['stock.quant'].search([])
+            for i in obj:
+                if i.location_id.location_id.name == 'AinW':
+                    k.ainw = i.inventory_quantity
+                else:
+                    k.ainw
+
+    def compute_ajm_location(self):
+        for k in self:
+            obj = self.env['stock.quant'].search([])
+            for i in obj:
+                if i.location_id.location_id.name == 'Ajm':
+                    k.ajm = i.inventory_quantity
+                    print('c', k.albr)
+                else:
+                    k.ajm
+
+    def compute_albr_location(self):
+        for k in self:
+            obj = self.env['stock.quant'].search([])
+            for i in obj:
+                if i.location_id.location_id.name == 'AlBr':
+                    k.albr = i.inventory_quantity
+                    print('b', k.albr)
+                else:
+                    k.albr
+
+    def compute_ban_location(self):
+        for k in self:
+            obj = self.env['stock.quant'].search([])
+            for i in obj:
+                if i.location_id.location_id.name == 'BAN':
+                    k.ban = i.inventory_quantity
+                    print(k.ban)
+                else:
+                    k.ban
+
+    def compute_abu_location(self):
+        for k in self:
+            obj = self.env['stock.quant'].search([])
+            for i in obj:
+                if i.location_id.location_id.name == 'ABU':
+                    k.abu = i.inventory_quantity
+                else:
+                    k.abu
+
+    def compute_abuold_location(self):
+        for k in self:
+            obj = self.env['stock.quant'].search([])
+            for i in obj:
+                print('pp', i.inventory_quantity)
+                if i.location_id.location_id.name == 'ABUOL':
+                    k.abuold = i.inventory_quantity
+                else:
+                    k.abuold
+
+    def compute_mzr_location(self):
+        for k in self:
+            obj = self.env['stock.quant'].search([])
+            for i in obj:
+                if i.location_id.location_id.name == 'MZR':
+                    k.mzr = i.inventory_quantity
+                else:
+                    k.mzr
+
+    def compute_sale_walk_in(self):
+        for i in self:
+            total = 0
+            obj = self.env['product.pricelist.item'].search([('name', '=', ''),
+                                                        ('pricelist_id.name', '=', 'Sale1')])
+            for j in obj:
+                total = j.fixed_price + total
+            i.sale1 = total
+
+    def compute_sale_credit(self):
+        for i in self:
+            total = 0
+            obj = self.env['product.pricelist.item'].search([('name', '=', ''),
+                                                             ('pricelist_id.name', '=', 'Sale2')])
+
+            for j in obj:
+                total = j.fixed_price+total
+            i.sale2 = total
+
+    def compute_sale_cash(self):
+        for i in self:
+            total = 0
+            obj = self.env['product.pricelist.item'].search([('name', '=', ''),
+                                                             ('pricelist_id.name', '=', 'Sale3')])
+
+            for j in obj:
+                total = j.fixed_price + total
+            i.sale3 = total
+
+
+
+
+
+
+
+
+
+
